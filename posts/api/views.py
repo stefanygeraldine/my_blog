cda7c7d1f4064c147e06f7f0cab59775d2008110
scarballo <scarballo@e-verse.com>
2022-08-19 21:22:07 -0300

    Add custom permission

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,10 +1,17 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from posts.models import Post
 from posts.api.serializers import PostSerializers
+from posts.api.permissions import IsAdminOrReadOnly
 
+
+class PostModelViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializers
+    queryset = Post.objects.all()
 
 # class PostApiView(APIView):
 # def get(self, request):
@@ -20,7 +27,3 @@ from posts.api.serializers import PostSerializers
 # serializer.is_valid(raise_exception=True)
 # serializer.save()
 # return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-class PostModelViewSet(ModelViewSet):
-    serializer_class = PostSerializers
-    queryset = Post.objects.all()
